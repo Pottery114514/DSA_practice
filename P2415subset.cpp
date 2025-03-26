d6544f71b02efee7b4406cebde28cd63e6a7b5f8
yoshino <pottery114514@gmail.com>
2025-03-26 20:55:37 +0800

    Update P2415subset.cpp

--- P2415subset.cpp
+++ P2415subset.cpp
@@ -2,7 +2,8 @@
 #include<vector>
 using namespace std;
 #define ll long long
-
+//有更直接的数学结论可以用,但一般的方法也想学会啊...
+//已经把之前自己写的全部推倒重来了,毕竟就自己写的那一坨,没多少参考价值.明明已经有了具体的思路了,也能人肉写出计算过程,但在程序化的时候总是写不出正确有条理的代码.是对语言不熟悉还是方法错了呢.
 void generateSubsets(vector<int>& nums, int index, vector<int>& current, ll& total) {
     if (index == nums.size()) {
         for (int num : current) {

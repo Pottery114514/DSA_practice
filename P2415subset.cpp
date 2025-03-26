a67c5cbee85de955f65567bdc1a7a09835eafca1
yoshino <pottery114514@gmail.com>
2025-03-26 20:41:19 +0800

    Update P2415subset.cpp

--- P2415subset.cpp
+++ P2415subset.cpp
@@ -1,11 +1,4 @@
 #include<iostream>
-#include<cmath>
-#include<string>
-#include<vector>
-#include<sstream>
-using namespace std;
-#define ll long long
-/*#include<iostream>
 #include<vector>
 using namespace std;
 #define ll long long
@@ -40,40 +33,5 @@ int main() {
         numbers.push_back(i);
     }
     result(numbers); // 输出24
-    return 0;
-}
-对于{1,2,3}
-1+(1+2)+(1+2+3)
- +(1+3)
-2+(2+3)
-3
-*/
-ll sub(int i,vector<int>& numbers,ll sum,int k){
-    int len=numbers.size();
-    int j=len;
-    ll curr=0;
-    if (k==len) return sum;
-    while (j<len){
-        curr+=numbers[j];
-        sum+=curr;
-        j++;
-    }
-    return sub(i,numbers,sum,k++);
-}
-void result(vector<int>& numbers){
-    ll res, sum;
-    for (int i=0;i<numbers.size();i++){
-        res+=sub(i,numbers,sum,0);
-    }
-    cout<<res;
-}
-int main(){
-    int num;
-    ll ans=0;
-    vector<int> numbers;
-    while (scanf("%d ",&num)!=EOF){
-        numbers.push_back(num);
-    }
-    result(numbers);
     return 0;
 }

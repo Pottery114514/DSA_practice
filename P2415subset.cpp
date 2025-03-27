61337a3a6accdbff8885471d7a1ab5b2b349538c
yoshino <pottery114514@gmail.com>
2025-03-27 20:52:21 +0800

    Update P2415subset.cpp

--- P2415subset.cpp
+++ P2415subset.cpp
@@ -5,34 +5,70 @@ using namespace std;
 //有更直接的数学结论可以用,但一般的方法也想学会啊...
 //已经把之前自己写的全部推倒重来了,毕竟就自己写的那一坨,没多少参考价值.明明已经有了具体的思路了,也能人肉写出计算过程,但在程序化的时候总是写不出正确有条理的代码.是对语言不熟悉还是方法错了呢.
 void generateSubsets(vector<int>& nums, int index, vector<int>& current, ll& total) {
-    if (index == nums.size()) {
+    //主要的时间开销在生成子集和计算总和的过程中。
+    if (index == nums.size()) {//直到最后一个之前不用管
         for (int num : current) {
             total += num;
         }
         return;
     }
-    ////好像有点懂了,像树一样延申下去
-    // 包含当前元素
+    //每个层级包含两种状态
     current.push_back(nums[index]);
     generateSubsets(nums, index + 1, current, total);
     current.pop_back();
-    
-    // 不包含当前元素
     generateSubsets(nums, index + 1, current, total);
 }
 
-void result(vector<int>& numbers) {
+void way1(vector<int>& numbers) {
     ll total = 0;
     vector<int> current;
     generateSubsets(numbers, 0, current, total);
     cout << total;
 }
-
-int main() {
-    vector<int> numbers;
-    for (int i = 1; i < 4; i++) {
-        numbers.push_back(i);
+ll power(int i,int p){
+    ll res=1;
+    while(p){
+        if (1&p%2){
+            res*=i;
+        }
+        i*=i;
+        p/=2;
     }
-    result(numbers); // 输出24
+    return res;
+}
+void way2(vector<int>&nums){
+   //根据选与不选,将子集用二进制数表示
+   /*
+   1->100
+   12->110
+   ...
+   3->001
+   目标为找出所有位数为len的二进制数,统计每一位上1的数量,将其与该位置的元素相乘即可
+   事实上可以发现每个元素出现的次数都为2^(n-1) (对称规律).该说是凑巧,还是必然的呢,
+   每个元素只有两种状态,用二进制数模拟似乎很自然.
+   这也说明了数学方法的重要性
+   */
+    ll len=nums.size();
+    ll sum=0;
+    if (len==1){
+        cout<<nums[0];
+        return;
+    }
+    
+    for (auto elem:nums){
+        sum+=elem;
+    }
+    cout<<sum*power(2,len-1);//不用pow是我能做到的最后一件事了
+
+}
+int main() {
+    int num;
+    vector<int> numbers;
+    while(scanf("%d ",&num)!=EOF){
+        numbers.push_back(num);
+    }
+    way2(numbers);
+    //result(numbers); // 输出24
+    //cout<<power(3,4);
     return 0;
 }
